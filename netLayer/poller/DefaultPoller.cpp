c12f58ec10b6d12f81f8b33988878529659bcbb2
zlonqi <zlonqi@163.com>
2021-06-06 20:36:30 +0800

    fix: net::poller::proxy function

--- netLayer/poller/DefaultPoller.cpp
+++ netLayer/poller/DefaultPoller.cpp
@@ -1,18 +1,18 @@
 //
-// Created by root on 2019/11/8.
+// Created by root on 2019/12/8.
 //
 
 #include "Poller.h"
 #include "PollPoller.h"
 #include "EPollPoller.h"
 
-#include <stdlib.h>
+#include <cstdlib>
 
 namespace tank{
     namespace net{
         Poller* Poller::newDefaultPoller(EventLoop* loop)
         {
-            if (::getenv("MUDUO_USE_POLL"))
+            if (::getenv("MUDUO_USE_POLL"))//from libmuduo, mit license.
             {
                 return new PollPoller(loop);
             }
