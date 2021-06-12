ce8d14490a24606f02df1dbb7b861e03101899c4
zlonqi <zlonqi@163.com>
2021-06-12 23:40:35 +0800

    fix: commit all

--- cmake-build-debug/cgiServer/CMakeFiles/fastcgi.dir/DependInfo.cmake
+++ cmake-build-debug/cgiServer/CMakeFiles/fastcgi.dir/DependInfo.cmake
@@ -5,8 +5,10 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
   "/root/longky/longky/cgiServer/cgiserver.cpp" "/root/longky/longky/cmake-build-debug/cgiServer/CMakeFiles/fastcgi.dir/cgiserver.cpp.o"
+  "/root/longky/longky/cgiServer/configLoad.cpp" "/root/longky/longky/cmake-build-debug/cgiServer/CMakeFiles/fastcgi.dir/configLoad.cpp.o"
   "/root/longky/longky/cgiServer/fastcgi.cpp" "/root/longky/longky/cmake-build-debug/cgiServer/CMakeFiles/fastcgi.dir/fastcgi.cpp.o"
   "/root/longky/longky/cgiServer/parseRequest.cpp" "/root/longky/longky/cmake-build-debug/cgiServer/CMakeFiles/fastcgi.dir/parseRequest.cpp.o"
+  "/root/longky/longky/cgiServer/start_up.cpp" "/root/longky/longky/cmake-build-debug/cgiServer/CMakeFiles/fastcgi.dir/start_up.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug/cgiServer/CMakeFiles/fastcgi.dir/cmake_clean.cmake
+++ cmake-build-debug/cgiServer/CMakeFiles/fastcgi.dir/cmake_clean.cmake
@@ -2,8 +2,10 @@ file(REMOVE_RECURSE
   "../../bin/fastcgi"
   "../../bin/fastcgi.pdb"
   "CMakeFiles/fastcgi.dir/cgiserver.cpp.o"
+  "CMakeFiles/fastcgi.dir/configLoad.cpp.o"
   "CMakeFiles/fastcgi.dir/fastcgi.cpp.o"
   "CMakeFiles/fastcgi.dir/parseRequest.cpp.o"
+  "CMakeFiles/fastcgi.dir/start_up.cpp.o"
 )
 
 # Per-language clean rules from dependency scanning.
