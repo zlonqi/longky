3928d4ededfa9eaa2af53120d16ecf1f1ce55574
zlonqi <zlonqi@163.com>
2021-06-12 23:36:07 +0800

    fix: webServer::redis Reformat Code

--- webServer/redis.cpp
+++ webServer/redis.cpp
@@ -4,24 +4,21 @@
 #include <iostream>
 #include <exception>
 
-namespace ccx{
+namespace ccx {
 
     using std::cout;
     using std::endl;
     using std::stringstream;
 
-    Redis::Redis(string ip,int port):
-    _ip(ip),
-    _port(port),
-     isRedisConnectWell_(false)
-    {
+    Redis::Redis(string ip, int port) :
+            _ip(ip),
+            _port(port),
+            isRedisConnectWell_(false) {
     }
 
-    bool Redis::Connect()
-    {
+    bool Redis::Connect() {
         _context = ::redisConnect(_ip.c_str(), _port);
-        if(_context && _context->err)
-        {
+        if (_context && _context->err) {
             //cout << "connect redis error" << endl;
             ::redisFree(_context);
             _context = nullptr;
@@ -29,89 +26,78 @@ namespace ccx{
             return false;
             //exit(EXIT_FAILURE);
         }
-         isRedisConnectWell_ = true;
+        isRedisConnectWell_ = true;
         return true;
         //cout << "redis Connect success" << endl;
     }
 
-    void Redis::disConnect()
-    {
+    void Redis::disConnect() {
         ::redisFree(_context);
         //cout << "redis disConnect success" << endl;
     }
 
-    string Redis::toString(long long value)
-    {
+    string Redis::toString(long long value) {
         char buf[32];
         snprintf(buf, sizeof buf, "%lld", value);
         return buf;
     }
-    string Redis::redisReplyToString(const redisReply* reply)
-    {
-        static const char* const types[] = { "",
-                                             "REDIS_REPLY_STRING", "REDIS_REPLY_ARRAY",
-                                             "REDIS_REPLY_INTEGER", "REDIS_REPLY_NIL",
-                                             "REDIS_REPLY_STATUS", "REDIS_REPLY_ERROR" };
+
+    string Redis::redisReplyToString(const redisReply *reply) {
+        static const char *const types[] = {"",
+                                            "REDIS_REPLY_STRING", "REDIS_REPLY_ARRAY",
+                                            "REDIS_REPLY_INTEGER", "REDIS_REPLY_NIL",
+                                            "REDIS_REPLY_STATUS", "REDIS_REPLY_ERROR"};
         string str = "";
         if (!reply) return str;
 
         if (reply->type == REDIS_REPLY_STRING ||
             reply->type == REDIS_REPLY_STATUS ||
-            reply->type == REDIS_REPLY_ERROR)
-        {
+            reply->type == REDIS_REPLY_ERROR) {
             str += string(reply->str, reply->len);
-        }
-        else if (reply->type == REDIS_REPLY_INTEGER)
-        {
+        } else if (reply->type == REDIS_REPLY_INTEGER) {
             str += toString(reply->integer);
-        }
-        else if (reply->type == REDIS_REPLY_ARRAY)
-        {
+        } else if (reply->type == REDIS_REPLY_ARRAY) {
             str += toString(reply->elements) + " ";
-            for (size_t i = 0; i < reply->elements; i++)
-            {
+            for (size_t i = 0; i < reply->elements; i++) {
                 str += " " + redisReplyToString(reply->element[i]);
             }
         }
         return str;
     }
-    void Redis::exeSET(char* key, char* value,int valenth){
+
+    void Redis::exeSET(char *key, char *value, int valenth) {
         int argc = 3;
-        const char *argv[] = {"set",key,value};
-        size_t argvlen[] = {3,32,(size_t)valenth};
-        try{
-            redisCommandArgv(_context,argc,argv,argvlen);
-        }catch(std::exception& e){
+        const char *argv[] = {"set", key, value};
+        size_t argvlen[] = {3, 32, (size_t) valenth};
+        try {
+            redisCommandArgv(_context, argc, argv, argvlen);
+        } catch (std::exception &e) {
             throw 1;
         }
     }
 
-    string Redis::exeGET(char* key){
-        int argc = 2;
-        const char *argv[] = {"get",key};
-        size_t argvlen[] = {3,32};
-        redisReply* reply =(redisReply*) redisCommandArgv(_context, 2, argv, argvlen);
+    string Redis::exeGET(const char *key) {
+        const char *argv[] = {"get", key};
+        size_t argvlen[] = {3, 32};
+        auto *reply = (redisReply *) redisCommandArgv(_context, 2, argv, argvlen);
         string value;
-        if(reply == nullptr) throw 0;
-        if(reply->type != REDIS_REPLY_NIL){
-            value =redisReplyToString(reply);
+        if (reply == nullptr) throw 0;
+        if (reply->type != REDIS_REPLY_NIL) {
+            value = redisReplyToString(reply);
         }
         freeReplyObject(reply);
         return value;
     }
-     void Redis::freeReply()
-    {
-        if(_reply)
-        {
+
+    void Redis::freeReply() {
+        if (_reply) {
             ::freeReplyObject(_reply);
-            _reply = NULL;
+            _reply = nullptr;
         }
     }
 
-    bool Redis::isError()
-    {
-        if(NULL == _reply)
-        {
+    bool Redis::isError() {
+        if (nullptr == _reply) {
             freeReply();
             disConnect();
             Connect();
--- webServer/redis.h
+++ webServer/redis.h
@@ -1,37 +1,43 @@
-#ifndef __REDIS_H__
-#define __REDIS_H__
+#ifndef REDIS_H
+#define REDIS_H
 
 #include <string>
-
 #include <hiredis/hiredis.h>
 
 using std::string;
 
+namespace ccx {
 
-namespace ccx{
-
-    class Redis
-    {
+    class Redis {
     public:
         Redis(string ip, int port);
+
     public:
         bool Connect();
+
         void disConnect();
+
     public:
-        string toString(long long value);
-        string redisReplyToString(const redisReply* reply);
-        void exeSET(char* key, char* value,int valenth);
-        string exeGET(char* key);
+        static string toString(long long value);
+
+        string redisReplyToString(const redisReply *reply);
+
+        void exeSET(char *key, char *value, int valenth);
+
+        string exeGET(const char *key);
+
     private:
         void freeReply();
+
         bool isError();
+
     public:
         bool isRedisConnectWell_;
     private:
         string _ip;
         int _port;
-        redisContext * _context;
-        redisReply * _reply;
+        redisContext *_context{};
+        redisReply *_reply{};
     };
 }
 
